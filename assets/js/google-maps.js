d7e89aa3ef68099431609b5af05d1d40b2ad6237
StudentConnect404 <85060394+StudentConnect404@users.noreply.github.com>
2021-06-03 10:34:26 +0530

    Update google-maps.js

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,6 +1,6 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: 23.038094, lng: 72.551650};
+    var myLatLng = {lat: 23.037516176282157, lng: 72.55280087110602};
 
     var map = new google.maps.Map(document.getElementById('myMap'), {
         zoom: 17,
